7a6b58a36066c44e52609c22478d27b6a8229269
Plums Maryjo <pmaryjo@mi-a5.msk.21-school.ru>
2022-03-12 21:35:35 +0300

    Zoom to center Save screen to file

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 package fractal;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.GregorianCalendar;
 
 class RGB{
     private final int r;
@@ -69,34 +72,62 @@ class Frame extends JFrame {
         MANDELBROT
     }
 
+    private double C_x;
+    private double C_y;
     private double zoom;
-    private double mouse_x;
-    private double mouse_y;
+    private double zoom_x;
+    private double zoom_y;
+    private double delta_x;
+    private double delta_y;
+    private FRACTAL fractal;
 
-    private final RGB black;
-    private final RGB white;
+    private RGB black;
+    private RGB color_1;
+    private RGB color_2;
+    private RGB color_3;
+    private RGB color_4;
+    private boolean CTRL;
+    private BufferedImage image;
+
     private final int width = 800;
-    private final FRACTAL fractal;
-    private final BufferedImage image;
 
     public Frame(String name) throws Exception {
+        initVars();
+        initFractal(name);
+        initColors();
+        setUpWindow();
+        setUpEvents();
+    }
+    private void initVars(){
+        zoom = 1.0;
+        zoom_x = 0.0;
+        zoom_y = 0.0;
+        delta_x = 0.0;
+        delta_y = 0.0;
+        CTRL = false;
+        image = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB);
+    }
+    private void initFractal(String name) throws Exception {
         if (name.equals("Julia")){
             fractal = FRACTAL.JULIA;
-            mouse_x = juliaToCartesian(-1);
-            mouse_y = juliaToCartesian(0);
+            C_x = juliaToCartesian(-1);
+            C_y = juliaToCartesian(0);
         } else if (name.equals("Mandelbrot")) {
             fractal = FRACTAL.MANDELBROT;
-            mouse_x = juliaToCartesian(0);
-            mouse_y = juliaToCartesian(0);
+            C_x = juliaToCartesian(0);
+            C_y = juliaToCartesian(0);
         } else {
             throw new Exception("Bad fractal name");
         }
-
-        zoom = 1.0;
-        image = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB);
+    }
+    private void initColors(){
         black = new RGB(0, 0, 0);
-        white = new RGB(255, 255, 255);
-
+        color_1 = new RGB(0, 15, 0);
+        color_2 = new RGB(0, 128, 0);
+        color_3 = new RGB(154, 215, 50);
+        color_4 = new RGB(173, 255, 47);
+    }
+    private void setUpWindow(){
         JPanel panel = new JPanel(){
             @Override
             protected void paintComponent(Graphics g){
@@ -112,33 +143,91 @@ class Frame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(panel);
-
+    }
+    private void setUpEvents(){
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
+                final double shift_delta = ((double)width / 30) / zoom;
                 final int keyCode = e.getKeyCode();
-                final double zoom_delta = 0.2;
 
                 if (keyCode == KeyEvent.VK_ESCAPE){
                     System.exit(0);
+                } else if (keyCode == KeyEvent.VK_S) {
+                    int a = JOptionPane.showConfirmDialog(null, "Save current image?", "", JOptionPane.YES_NO_OPTION);
+                    if (a == JOptionPane.YES_OPTION){
+                        makeScreenshot();
+                    }
+                    return;
+                } else if (keyCode == KeyEvent.VK_CONTROL){
+                    CTRL = true;
+                    return;
                 } else if (keyCode == KeyEvent.VK_UP){
-                    zoom += zoom_delta;
+                    delta_y -= shift_delta;
                 } else if (keyCode == KeyEvent.VK_DOWN){
-                    zoom -= zoom_delta;
+                    delta_y += shift_delta;
+                } else if (keyCode == KeyEvent.VK_LEFT){
+                    delta_x -= shift_delta;
+                } else if (keyCode == KeyEvent.VK_RIGHT){
+                    delta_x += shift_delta;
                 }
                 draw();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                final int keyCode = e.getKeyCode();
+
+                if (keyCode == KeyEvent.VK_CONTROL){
+                    CTRL = false;
+                }
+            }
+        });
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                final int button = e.getButton();
+
+                if (button == MouseEvent.BUTTON1) {
+                    if (fractal == FRACTAL.MANDELBROT) {
+                        return;
+                    }
+                    C_x = e.getX();
+                    C_y = e.getY();
+                    draw();
+                }
             }
         });
 
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                super.mouseMoved(e);
-                if (fractal == FRACTAL.MANDELBROT){
+                if (!CTRL || fractal == FRACTAL.MANDELBROT){
                     return;
                 }
-                mouse_x = e.getX();
-                mouse_y = e.getY();
+
+                C_x = e.getX();
+                C_y = e.getY();
+                draw();
+            }
+        });
+
+        addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                final double zoom_delta = 2;
+                final int rotation = e.getWheelRotation();
+
+                zoom_x = e.getX();
+                zoom_y = e.getY();
+
+                if (rotation < 0){
+                    zoom *= zoom_delta;
+                } else {
+                    zoom /= zoom_delta;
+                }
+
                 draw();
             }
         });
@@ -156,13 +245,13 @@ class Frame extends JFrame {
         final int max_iters = 50;
 
         Complex Z = new Complex();
-        final Complex C = new Complex(cartesianToJulia(mouse_x),
-                                        cartesianToJulia(mouse_y));
+        final Complex C = new Complex(cartesianToJulia(C_x),
+                                        cartesianToJulia(C_y));
 
         for (int y = 0; y < width; y++){
             for (int x = 0; x < width; x++){
-                Z.setRe(cartesianToJulia(x));
-                Z.setIm(cartesianToJulia(y));
+                Z.setRe(cartesianToJulia(x + delta_x * zoom) / zoom);
+                Z.setIm(cartesianToJulia(y + delta_y * zoom) / zoom);
 
                 int iters = 0;
                 while (iters < max_iters) {
@@ -173,18 +262,11 @@ class Frame extends JFrame {
                     iters++;
                 }
 
-                int rgb;
-                if (iters == max_iters){
-                    rgb = white.toInt();
-                } else {
-                    rgb = black.toInt();
-                }
-                image.setRGB(x, y, rgb);
+                image.setRGB(x, y, itersToColor(iters, max_iters));
             }
         }
         repaint();
     }
-
     private void mandelbrot(){
         final int max_iters = 50;
 
@@ -192,40 +274,91 @@ class Frame extends JFrame {
         final Complex C = new Complex();
 
         for (int y = 0; y < width; y++){
-        for (int x = 0; x < width; x++){
-            Z.setRe(0);
-            Z.setIm(0);
+            for (int x = 0; x < width; x++){
+                Z.setRe(0);
+                Z.setIm(0);
 
-            C.setRe(cartesianToJulia(x));
-            C.setIm(cartesianToJulia(y));
+                C.setRe(cartesianToJulia(x));
+                C.setIm(cartesianToJulia(y));
 
-            int iters = 0;
-            while (iters < max_iters) {
-                Z = Complex.sum(Complex.pow_2(Z), C);
-                if (Z.getModulus() > 2.0){
-                    break;
+                int iters = 0;
+                while (iters < max_iters) {
+                    Z = Complex.sum(Complex.pow_2(Z), C);
+                    if (Z.getModulus() > 2.0){
+                        break;
+                    }
+                    iters++;
                 }
-                iters++;
-            }
 
-            int rgb;
-            if (iters == max_iters){
-                rgb = white.toInt();
-            } else {
-                rgb = black.toInt();
+                image.setRGB(x, y, itersToColor(iters, max_iters));
             }
-            image.setRGB(x, y, rgb);
-        }
         }
         repaint();
     }
+    private int itersToColor(int iters, int max_iters){
+        if (iters == max_iters){
+            return black.toInt();
+        } else if (iters < max_iters / 4) {
+            return color_1.toInt();
+        } else if (iters < max_iters * 2 / 4) {
+            return color_2.toInt();
+        } else if (iters < max_iters * 3 / 4) {
+            return color_3.toInt();
+        } else {
+            return color_4.toInt();
+        }
+    }
 
     private double cartesianToJulia(double a){
-        return -1.0 * 2.0 + 4.0 / width * a;
+        return -2.0 + 4.0 / width * a;
     }
     private double juliaToCartesian(double a){
         return (a + 2.0) * width / 4.0;
     }
+
+    private void makeScreenshot(){
+        final String extension = "jpg";
+        final String path = System.getProperty("user.home") + "/Desktop/" + getDate() + "." + extension;
+
+        try {
+            ImageIO.write(image, extension, new File(path));
+            JOptionPane.showMessageDialog(null, "Done!",
+                                        null, JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage(),
+                                        "Error!", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private String getDate(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.DAY_OF_MONTH)));
+        builder.append('.');
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.MONTH) + 1));
+        builder.append('.');
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.YEAR)));
+        builder.append('-');
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.HOUR_OF_DAY)));
+        builder.append('.');
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.MINUTE)));
+        builder.append('.');
+        builder.append(parseDateInt(calendar.get(GregorianCalendar.SECOND)));
+        return builder.toString();
+    }
+    private String parseDateInt(int val_int){
+        String val_str = String.valueOf(val_int);
+
+        if (val_int >= 10){
+            return val_str;
+        }
+        return "0" + val_str;
+    }
+
+    private double modulus(double a){
+        return (a < 0.0) ? a * -1.0 : a;
+    }
+
 }
 
 public class Main {
